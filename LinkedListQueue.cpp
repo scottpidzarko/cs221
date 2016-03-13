2efc2e6bb580fd7ac9c68ac4b214373af1fa579f
unknown <w2p8@WIN01-ICCS-X050.use.cs.ubc.ca>
2016-03-12 18:46:23 -0800

    initial commit

---
+++ LinkedListQueue.cpp
@@ -0,0 +1,35 @@
+//LinkedListQueue.cpp
+#ifndef _LINKEDLISTQUEUE_CPP
+#define _LINKEDLISTQUEUE_CPP
+
+#include "LinkedListQueue.hpp"
+#include <cstdlib> //for NULL
+#include <cassert>
+#include <iostream>
+LinkedListQueue::LinkedListQueue()
+{
+  // TODO:  Initialize any member variables as needed in the constructor.
+}
+
+void LinkedListQueue::add(MazeState *elem)
+{
+  // TODO:  Implement this.
+}
+
+MazeState *LinkedListQueue::remove()
+{
+  // TODO:  Implement this.
+}
+
+bool LinkedListQueue::is_empty()
+{
+  // TODO:  Implement this.
+}
+
+LinkedListQueue::~LinkedListQueue()
+{
+  // TODO:  Implement the destructor.  Be sure to delete everything.
+}
+
+#endif
+
